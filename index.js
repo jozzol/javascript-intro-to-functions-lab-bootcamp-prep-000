97be3eb8dbf13480938aafdba2604e25240bbc53
Jose Guillermo Garcia Serrano <joseg1224@hotmail.com>
2018-06-20 03:14:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,11 +14,9 @@ function sayHiToGrandma(string){
   if (string.toLowerCase === string)
     return "i can't hear you!"
 }
-function sayHiToGrandma(string){
   if (string.toUpperCase === string)
     return "YES INDEED!"
 }
-function sayHiToGrandma(string){
   if (string === "I love you, Grandma.")
     return "I love you, too."
 }
